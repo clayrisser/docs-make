2a2cc8c9df46ff0175f9cf25c086cce60434ae87
Clay Risser <email@clayrisser.com>
2021-06-14 22:19:07 -0500

    Extract document

--- src/bin/cleanPandoc.ts
+++ src/bin/cleanPandoc.ts
@@ -1,17 +1,12 @@
 import fs from 'fs-extra';
+import { load } from 'cheerio';
 import path from 'path';
 
 (async () => {
   if (process.argv.length < 3) throw new Error('missing file path');
   const filePath = path.resolve(process.cwd(), process.argv[2]);
   let body = (await fs.readFile(filePath)).toString();
-  body = body.replace(/^<div(>|\s).*\n?/g, '');
-  body = body.replace(/\n<div(>|\s).*\n?/g, '');
-  body = body.replace(/\n<\/div>.*\n?/g, '');
-  body = body.replace(
-    /# \[.+]\(\.\.\/.+\.html\)(\n|\s)+### Navigation(.|\n)*/g,
-    ''
-  );
-  body = body.replace(/<a href="[^>]+>(\s|\n)*¶(\s|\n)*<\/a>/g, '');
+  const $ = load(body);
+  body = $('body .document .body').html() || body;
   await fs.writeFile(filePath, body);
 })();
